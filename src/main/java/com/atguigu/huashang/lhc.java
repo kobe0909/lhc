66bb48a81cff4e8192095093566a57dfeb20ddea
liuchongyang <835486902@qq.com>
2020-07-10 20:19:36 +0800

    lhc

--- src/main/java/com/atguigu/huashang/lhc.java
+++ src/main/java/com/atguigu/huashang/lhc.java
@@ -12,6 +12,9 @@ public class lhc {
         System.out.println("gudu123");
         System.out.println("gudu123");
         System.out.println("gudu123");
+        System.out.println("gudu123");
+        System.out.println("gudu123");
+        System.out.println("gudu123");
 
         //红色 未追踪代码  绿色 ：暂存区  黑色：已提交本地库 蓝色 ：已提交本地库后 又发生了修改且未提交
     }
