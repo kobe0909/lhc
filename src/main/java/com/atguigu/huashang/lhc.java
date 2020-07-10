e54fac5e2460839858a6f1e7a6b6eb3a3c343188
liuchongyang <835486902@qq.com>
2020-07-10 16:01:06 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/huashang/lhc.java
@@ -0,0 +1,16 @@
+package com.atguigu.huashang;
+
+/**
+ * @author lcy
+ * @create 2020-07-10 15:53
+ */
+public class lhc {
+
+    public static void main(String[] args) {
+        System.out.println("gudu123");
+        System.out.println("gudu123");
+        System.out.println("gudu123");
+        System.out.println("gudu123");
+        System.out.println("gudu123");
+    }
+}
